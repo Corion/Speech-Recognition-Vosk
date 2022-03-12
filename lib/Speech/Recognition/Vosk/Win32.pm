5e1b6ad2ce1bf79bfd0795c63643765440c6b9a2
Max Maischein <corion@corion.net>
2022-03-12 12:09:48 +0100

    Put the loading of subroutines into a subroutine

--- lib/Speech/Recognition/Vosk/Win32.pm
+++ lib/Speech/Recognition/Vosk/Win32.pm
@@ -3,33 +3,37 @@ use strict;
 use 5.012;
 use File::Basename 'dirname';
 use Win32::API;
-use Exporter 'import';
+require Exporter;
+use File::ShareDir 'dist_dir';
 
 # We want to load the DLLs from here:
-$ENV{PATH} .= ";" . dirname($0) . "/vosk-win64-0.3.32";
+sub load_libvosk {
+    my ($path) = @_;
+    $path //= dist_dir('Speech::Recognition::Vosk');
+    local $ENV{PATH} .= ";" . $path;
 
-#our $model_new = Win32::API->new("libvosk.dll", "vosk_model_new", "P", "N")
-Win32::API::More->Import("libvosk.dll", "vosk_model_new", "P", "N")
-    or die $^E;
+    #our $model_new = Win32::API->new("libvosk.dll", "vosk_model_new", "P", "N")
+    Win32::API::More->Import("libvosk.dll", "vosk_model_new", "P", "N")
+        or die $^E;
 
-Win32::API::More->Import("libvosk.dll", "vosk_model_find_word", "NP", "N")
-    or die $^E;
-    
-Win32::API::More->Import("libvosk.dll", "vosk_recognizer_new", "NF", "N")
-    or die $^E;
+    Win32::API::More->Import("libvosk.dll", "vosk_model_find_word", "NP", "N")
+        or die $^E;
 
-Win32::API->Import("libvosk.dll", "vosk_recognizer_accept_waveform", "NPN", "N")
-    or die $^E;
+    Win32::API::More->Import("libvosk.dll", "vosk_recognizer_new", "NF", "N")
+        or die $^E;
 
-Win32::API->Import("libvosk.dll", "vosk_recognizer_partial_result", "N", "P")
-    or die $^E;
+    Win32::API->Import("libvosk.dll", "vosk_recognizer_accept_waveform", "NPN", "N")
+        or die $^E;
 
-Win32::API->Import("libvosk.dll", "vosk_recognizer_result", "N", "P")
-    or die $^E;
+    Win32::API->Import("libvosk.dll", "vosk_recognizer_partial_result", "N", "P")
+        or die $^E;
 
-Win32::API->Import("libvosk.dll", "vosk_recognizer_final_result", "N", "P")
-    or die $^E;
+    Win32::API->Import("libvosk.dll", "vosk_recognizer_result", "N", "P")
+        or die $^E;
 
+    Win32::API->Import("libvosk.dll", "vosk_recognizer_final_result", "N", "P")
+        or die $^E;
+}
 
 our @EXPORT_OK = (qw(
     vosk_model_new
@@ -40,5 +44,10 @@ our @EXPORT_OK = (qw(
     vosk_model_recognizer_result
     vosk_model_recognizer_final_result
 ));
+
+sub import {
+    load_libvosk();
+    goto &Exporter::import;
+};
 
 1;
